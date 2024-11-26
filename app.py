c390b1f4dc15206c6b58512fc07b7939066d8842
TheTrollGodJr <thetrollgodjr@gmail.com>
2024-11-26 15:58:07 -0700

    Added credentials env file. Modified app.py to work with the env

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv(os.path.join(os.path.dirname(os.path.realpath(__file__)), "credentials.env"))
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    #print()
+    #exit(0)
+    host = os.getenv("HOST")
+    debug = os.getenv("DEBUG", "false").lower() == "true"
+    port = int(os.getenv("PORT"))
+    #print(f"{host}:{port}")
+
+    app.run(host=host, debug=debug, port=port)
